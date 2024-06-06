4a62f9d99598e7401a93d51b2dd62f4d3cd3a85e
fliyyer <rahmatcube15@gmail.com>
2024-06-06 20:12:46 +0700

    Add CSS to components

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,21 @@ function App() {
   console.log(todos)
 
   return (
-    <div>
-      <h1>My Todo List</h1>
-      {/* {todos.map((todo) => {
-        return <p key={todo.id}>{todo.title}</p>
-      })} */}
+    <div style={styles.container}>
+      <h1 style={styles.title}>My Todo List</h1>
       <Todos todos={todos} />
     </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize: '36px',
+  },
 }
 
 export default App
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import TodoItem from './TodoItem'
 const Todos = ({ todos }) => {
     return (
-        <div>
+        <div style={styles.container}>
             {todos.map((todo) => {
                 return <TodoItem key={todo.id} todo={todo} />
             })}
         </div>
     )
+}
+
+const styles = {
+    container: {
+        width: '40%',
+        margin: '0 auto',
+    },
 }
 
 export default Todos
