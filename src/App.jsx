abf97cc487a0f601f48bf90a6e757b72f1cc7084
fliyyer <rahmatcube15@gmail.com>
2024-06-06 20:34:16 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,20 @@ function App() {
     },
   ])
 
-  console.log(todos)
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   )
 }
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import TodoItem from './TodoItem'
-const Todos = ({ todos }) => {
+const Todos = ({ todos, toggleCompleted }) => {
     return (
         <div style={styles.container}>
             {todos.map((todo) => {
-                return <TodoItem key={todo.id} todo={todo} />
+                return <TodoItem key={todo.id} toggleCompleted={toggleCompleted} todo={todo} />
             })}
         </div>
     )
